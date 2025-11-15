ff8b93abf8fb8c3eb302b0e09c6ac1eb4ec4d196
Lexate <47445605+Lexate@users.noreply.github.com>
2025-11-15 20:08:45 +0100

    Main functionality working

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
 #![expect(dead_code)]
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::layout::{Constraint, Flex, Layout};
+use ratatui::style::Stylize;
+use ratatui::text::Text;
 use ratatui::widgets::{Block, Borders, Paragraph};
 use ratatui::{DefaultTerminal, Frame, layout::Position, layout::Rect, widgets::Widget};
 use ratatui_code_editor::editor::Editor;
 use ratatui_code_editor::theme::vesper;
-use std::io;
 use std::io::{self, Write};
-use std::rc::Rc;
+use std::mem::swap;
 
 use regex::Regex;
 use std::fs::{self, File};
 use std::process::{Command, Output};
+
 fn main() -> anyhow::Result<()> {
     if !test_compilers() {
         println!("You need to have Bun and python3 installed to run this game");
     }
 
-    let utp = compiler(
-        "hello".to_string(),
-        "print('helawlo!')".to_string(),
-        "python3",
-    );
+    let utp = compiler::<languages::Python>("hello".to_string(), "print('helawlo!')".to_string());
     println!("output: {}, success: {}", utp.output, utp.success);
 
-    let mut terminal = ratatui::init();
-    let mut app = App::new();
+    let prob = Problem {
+        request: "Make the program output \"Hello test!\"".to_string(),
+        initial_problem: "fn main() {\n    println!(\"Hello, world!\");\n}".to_string(),
+        check_regex: "Hello test!".to_string(),
+    };
 
-    // let app_result = app.run(&mut terminal);
+    let mut terminal = ratatui::init();
+    let mut app = App::new(prob);
+
+    let app_result = app.run(&mut terminal);
     ratatui::restore();
     // app_result
     println!("{}", app.get_editor_content());
@@ -39,19 +43,21 @@ struct App {
     editor_area: Rect,
     counter: u8,
     exit: bool,
+    problem: Problem,
+    output: String,
+    correct: bool,
 }
 
 impl App {
-    fn new() -> Self {
+    fn new(problem: Problem) -> Self {
         App {
-            editor: Editor::new(
-                "rust",
-                "fn main() {\n    println!(\"Hello, world!\");\n}",
-                vesper(),
-            ),
+            editor: Editor::new("rust", &problem.initial_problem, vesper()),
             editor_area: Rect::default(),
             counter: 0,
             exit: false,
+            problem,
+            output: "Press f5 to check your code against the solution.".to_string(),
+            correct: false,
         }
     }
 
@@ -110,8 +116,14 @@ impl App {
         self.exit = true;
     }
 
-    fn check(&self) {
-        todo!()
+    fn check(&mut self) {
+        let result = compiler::<languages::Rust>(
+            self.problem.check_regex.clone(),
+            self.get_editor_content(),
+        );
+
+        self.output = result.output;
+        self.correct = result.success;
     }
 
     fn get_editor_content(&self) -> String {
@@ -120,6 +132,15 @@ impl App {
 
     fn set_editor_content(&mut self, content: &str) {
         self.editor.set_content(content);
+    }
+
+    fn get_status_bar(&self) -> Text {
+        let status = Text::raw(format!(
+            "Score: {}, Sucess: {}",
+            self.problem.diff(self.get_editor_content()),
+            self.correct
+        ));
+        status
     }
 }
 
@@ -135,30 +156,63 @@ impl Widget for &App {
             Constraint::Max(1),
         ])
         .split(area);
-        Paragraph::new("problem")
+        Paragraph::new(self.problem.request.clone())
             .centered()
             .block(Block::bordered().borders(Borders::BOTTOM))
             .render(areas[0], buf);
 
         self.editor.render(areas[1], buf);
 
-        Paragraph::new(self.get_editor_content())
+        Paragraph::new(self.output.clone())
             .block(Block::bordered().title("Output"))
             .render(areas[2], buf);
 
-        Paragraph::new("Stats").render(areas[3], buf);
+        Paragraph::new(self.get_status_bar()).render(areas[3], buf);
     }
 }
 
 struct Problem {
+    request: String,
     initial_problem: String,
     check_regex: String,
 }
 
 impl Problem {
     fn diff(&self, comparison: String) -> usize {
-        todo!()
+        minimum_edit_distance(&self.initial_problem, &comparison)
     }
+}
+
+fn minimum_edit_distance(s1: &str, s2: &str) -> usize {
+    let s: Vec<char> = s1.chars().collect();
+    let t: Vec<char> = s2.chars().collect();
+    let s_len = s.len();
+    let t_len = t.len();
+
+    let mut v0: Vec<usize> = (0..t_len + 1).collect();
+    let mut v1: Vec<usize> = vec![0; t_len + 1];
+
+    let mut substitution_cost: usize;
+
+    for i in 0..s_len {
+        v1[0] = i + 1;
+
+        for j in 0..t_len {
+            let deletion_cost = v0[j + 1] + 1;
+            let insertion_cost = v1[j] + 1;
+
+            if s[i] == t[j] {
+                substitution_cost = v0[j];
+            } else {
+                substitution_cost = v0[j] + 1;
+            }
+
+            v1[j + 1] = deletion_cost.min(insertion_cost).min(substitution_cost)
+        }
+
+        swap(&mut v0, &mut v1);
+    }
+    v0[t_len]
 }
 
 fn test_compilers() -> bool {
@@ -188,14 +242,39 @@ struct CompilerReturn {
     success: bool,
     output: String,
 }
-fn compiler(regex: String, input: String, language: &str) -> CompilerReturn {
+
+mod languages {
+    pub trait Language {
+        fn format_command(file_name: &str) -> String;
+    }
+    pub struct Python;
+
+    impl Language for Python {
+        fn format_command(file_name: &str) -> String {
+            format!("python3 {file_name}")
+        }
+    }
+
+    pub struct Rust;
+
+    impl Language for Rust {
+        fn format_command(file_name: &str) -> String {
+            format!("rustc {file_name} -o temp_prog && ./temp_prog")
+        }
+    }
+}
+
+fn compiler<L>(regex: String, input: String) -> CompilerReturn
+where
+    L: languages::Language,
+{
     let name = "temp.tmp";
     let mut file = File::create(name).unwrap();
     write!(file, "{}", input).unwrap();
 
     let utp = Command::new("sh")
         .arg("-c")
-        .arg(format!("{language} {name}"))
+        .arg(L::format_command(name))
         .output()
         .unwrap();
 
